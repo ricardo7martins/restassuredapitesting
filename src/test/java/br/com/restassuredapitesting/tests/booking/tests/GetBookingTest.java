569b20c6836c5270e8b0e06ce57bee795acfd10e
Ricardo Martins <ricardo.martins@edu.pucrs.br>
2021-10-19 17:54:21 -0300

    cria classe Utils e validador de schema para bookings

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
@@ -3,9 +3,13 @@ package br.com.restassuredapitesting.tests.booking.tests;
 import br.com.restassuredapitesting.base.BaseTests;
 import br.com.restassuredapitesting.suites.AllTests;
 import br.com.restassuredapitesting.tests.booking.requests.GetBookingRequest;
+import br.com.restassuredapitesting.utils.Utils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.io.File;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class GetBookingTest extends BaseTests {
@@ -19,6 +23,15 @@ public class GetBookingTest extends BaseTests {
                 .then()
                 .statusCode(200)
                 .body("size()", greaterThan(0));
+    }
+
+    @Test
+    @Category(AllTests.class)
+    public void validaSchemaDaListagemDeReservas() {
+        getBookingRequest.bookingReturnIds()
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchema(new File(Utils.getSchemaBasePath("booking", "bookings"))));
     }
 
 }
