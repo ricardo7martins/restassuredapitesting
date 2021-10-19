2f28d940de7d32372d9f0a94f978af8b40678a27
Ricardo Martins <ricardo.martins@edu.pucrs.br>
2021-10-19 20:31:16 -0300

    adiciona suites ao desafio

--- src/test/java/br/com/restassuredapitesting/runners/SmokeTests.java
+++ src/test/java/br/com/restassuredapitesting/runners/SmokeTests.java
@@ -2,13 +2,16 @@ package br.com.restassuredapitesting.runners;
 
 
 import br.com.restassuredapitesting.tests.auth.tests.PostAuthTest;
+import br.com.restassuredapitesting.tests.ping.tests.GetPingTest;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Categories.class)
-@Categories.IncludeCategory(br.com.restassuredapitesting.suites.SmokeTests.class)
+@Categories.IncludeCategory({br.com.restassuredapitesting.suites.SmokeTests.class,
+        br.com.restassuredapitesting.suites.HealthCheckTests.class})
 @Suite.SuiteClasses({
+        GetPingTest.class,
         PostAuthTest.class
 })
 public class SmokeTests {
